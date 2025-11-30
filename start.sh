22c283940fa38ae0c7878a7279a23ea8538e03f4
Richard <richard@rossberg.info>
2025-11-30 15:38:21 +0100

    🚧 fix Backup on start

--- start.sh
+++ start.sh
@@ -6,14 +6,14 @@ if [ -n "$BACKUP_SCHEDULE" ]; then
     printf "BACKUP_SCHEDULE detected: $BACKUP_SCHEDULE\n"
     printf "\n"
 
-    # create crontab
-    echo "$BACKUP_SCHEDULE /usr/local/bin/python3 /code/main.py >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
-    crond -f -L /dev/stdout
-
     # Check if Backup_ON_STARTUP is set to run a backup on startup
     if [ "$(echo "$BACKUP_ON_STARTUP" | tr '[:upper:]' '[:lower:]')" = "true" ]; then
         /usr/local/bin/python3 /code/main.py
     fi
+
+    # create crontab
+    echo "$BACKUP_SCHEDULE /usr/local/bin/python3 /code/main.py >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
+    crond -f -L /dev/stdout
 
 else
     printf "No BACKUP_SCHEDULE detected. Starting backup ..."
